784f4897a1b034137c19324b6b0305a1b46d13cd
andresochoa91 <andres_ochoa91@hotmail.com>
2020-11-20 22:14:33 -0800

    Location model added and relationship set up

---
+++ db/migrate/20201121060936_create_locations.rb
@@ -0,0 +1,16 @@
+class CreateLocations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :locations do |t|
+      t.string :country
+      t.string :state
+      t.string :city
+      t.string :address
+      t.integer :zip_code
+      t.text :details
+      t.integer :user_id
+
+      t.timestamps
+    end
+    add_index :locations, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_220352) do
+ActiveRecord::Schema.define(version: 2020_11_21_060936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,19 @@ ActiveRecord::Schema.define(version: 2020_11_20_220352) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_customers_on_user_id"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "country"
+    t.string "state"
+    t.string "city"
+    t.string "address"
+    t.integer "zip_code"
+    t.text "details"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
   create_table "merchants", force: :cascade do |t|
