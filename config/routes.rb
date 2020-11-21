fa1c7b2caf953de2276a92de057f6c4b44e3319e
andresochoa91 <andres_ochoa91@hotmail.com>
2020-11-20 23:40:37 -0800

    Some clues in product create method

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :users
   resources :customers
   resources :merchants
+  resources :products
+  
   post "login", to: "sessions#create"
   delete "login", to: "sessions#destroy"
 end
