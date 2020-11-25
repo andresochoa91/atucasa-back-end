444c3a48f26b14cd0e431e26b21f916f99d083d0
andresochoa91 <andres_ochoa91@hotmail.com>
2020-11-25 13:34:12 -0800

    Order model added

---
+++ db/migrate/20201125213316_create_orders.rb
@@ -0,0 +1,16 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.integer :customer_id
+      t.integer :merchant_id
+      t.boolean :accepted
+      t.string :current_user
+      t.float :tip
+      t.float :delivery_fee
+
+      t.timestamps
+    end
+    add_index :orders, :customer_id
+    add_index :orders, :merchant_id
+  end
+end
