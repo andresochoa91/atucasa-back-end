5220ffb7d3ed3c00721ab1245eacb4e227045050
andresochoa91 <andres_ochoa91@hotmail.com>
2020-11-30 19:31:31 -0800

    Routes fixed Before creating controller for product_orders Continue fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,51 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/show'
-  get 'orders/create'
-  get 'orders/update'
-  get 'orders/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :customers
-  resources :merchants
-  resources :products
-  resources :links
+  # resources :users
+  # resources :customers
+  # resources :merchants
+  # resources :products
+  # resources :links
 
   #User Endpoints
-  post '/login', to: 'sessions#create'
-  post '/signup', to: 'users#create'
+  post '/sign_up', to: 'users#create'
+  post '/log_in', to: 'sessions#create'
+  delete '/log_out', to: 'sessions#destroy'
   get '/current_user', to: 'users#show'
-  put '/current_user', to: 'users#update'
-  delete '/current_user', to: 'sessions#destroy'
+  put '/current_user/update', to: 'users#update'
 
   #Location Endpoints
   get '/current_user/location', to: 'locations#show'
   put '/current_user/location', to: 'locations#update'
 
   #Customer Endpoints
-  get '/current_user/customer_data', to: 'customers#show'
-  put '/current_user/customer_data', to: 'customers#update'
+  get '/current_user/customer', to: 'customers#show'
+  put '/current_user/customer', to: 'customers#update'
 
   #Merchant Endpoints
-  get '/current_user/customer_data/merchants', to: 'merchants#index'
-  get '/current_user/customer_data/merchants/:id', to: 'merchants#show'
-  get '/current_user/merchant_data', to: 'merchants#show_merchant_data'
-  put '/current_user/merchant_data', to: 'merchants#update'
+  get '/current_user/customer/merchants', to: 'merchants#index'
+  get '/current_user/customer/merchants/:id', to: 'merchants#show'
+  get '/current_user/merchant', to: 'merchants#show_merchant_data'
+  put '/current_user/merchant', to: 'merchants#update'
 
   #Products Endpoints
-  get '/current_user/merchant_data/products', to: 'products#index'
-  get '/current_user/merchant_data/products/:id', to: 'products#show'
-  post '/current_user/merchant_data/products', to: 'products#create'
-  put '/current_user/merchant_data/products/:id', to: 'products#update'
-  delete '/current_user/merchant_data/products/:id', to: 'products#destroy'
+  get '/current_user/merchant/products', to: 'products#index'
+  get '/current_user/merchant/products/:id', to: 'products#show'
+  post '/current_user/merchant/products', to: 'products#create'
+  put '/current_user/merchant/products/:id', to: 'products#update'
+  delete '/current_user/merchant/products/:id', to: 'products#destroy'
 
   #Links Endpoints
-  get '/current_user/merchant_data/links', to: 'links#index'
-  get '/current_user/merchant_data/links/:id', to: 'links#show'
-  post '/current_user/merchant_data/links', to: 'links#create'
-  put '/current_user/merchant_data/links/:id', to: 'links#update'
-  delete '/current_user/merchant_data/links/:id', to: 'links#destroy'
+  get '/current_user/merchant/links', to: 'links#index'
+  get '/current_user/merchant/links/:id', to: 'links#show'
+  post '/current_user/merchant/links', to: 'links#create'
+  put '/current_user/merchant/links/:id', to: 'links#update'
+  delete '/current_user/merchant/links/:id', to: 'links#destroy'
 
   #Orders Endpoints
-  get '/current_user/customer_data/merchants/:merchant_id/orders', to: 'orders#index'
-  get '/current_user/customer_data/merchants/:merchant_id/orders/:id', to: 'orders#show'
-  post '/current_user/customer_data/merchants/:merchant_id/orders', to: 'orders#create'
-  # post '/current_user/merchant_data/links', to: 'links#create'
-  # put '/current_user/merchant_data/links/:id', to: 'links#update'
-  # delete '/current_user/merchant_data/links/:id', to: 'links#destroy'
+  get '/current_user/customer/merchants/:merchant_id/orders', to: 'orders#index'
+  get '/current_user/customer/merchants/:merchant_id/orders/:id', to: 'orders#show'
+  post '/current_user/customer/merchants/:merchant_id/orders', to: 'orders#create'
+  # post '/current_user/merchant/links', to: 'links#create'
+  # put '/current_user/merchant/links/:id', to: 'links#update'
+  # delete '/current_user/merchant/links/:id', to: 'links#destroy'
 end
