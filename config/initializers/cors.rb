38b47e1b3e5bab28721ab30463a1cb34a78bfbba
andresochoa91 <andres_ochoa91@hotmail.com>
2020-12-02 12:44:29 -0800

    After changing API route

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://atucasa.netlify.app/signup'
+    origins 'https://atucasa.netlify.app'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
