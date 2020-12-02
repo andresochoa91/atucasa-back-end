344fb7457869fbd1249ff23edd7bcccdad0e9f15
andresochoa91 <andres_ochoa91@hotmail.com>
2020-12-02 14:53:39 -0800

    Set up

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,8 @@ module Atupuerta
     config.middleware.insert_before 0, Rack::Cors, debug: true do
       allow do
         origins '*'
-        resource '*', 
+        resource '*',
+          credentials: true, 
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
