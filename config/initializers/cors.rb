56ea4438e6bbfb18ab1628982a09036e4c3e114d
andresochoa91 <andres_ochoa91@hotmail.com>
2020-12-02 11:56:55 -0800

    CORS set for alive atucasa app, fixing bug

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3000,
               https://localhost:3000,
-              https://atupuerta.netlify.app'
+              https://atucasa.netlify.app'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
