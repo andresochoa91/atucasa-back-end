128c70e2ca9b6d68005ae43e3f3470c1ba472de9
andresochoa91 <andres_ochoa91@hotmail.com>
2020-12-02 13:04:47 -0800

    Still setting CORS up

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,6 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
     resource '*',
+      credentials: Access-Control-Allow-Credentials,
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
