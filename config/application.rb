c028bad205f84450e4aee2948e31cee7f225898e
andresochoa91 <andres_ochoa91@hotmail.com>
2020-12-02 00:03:41 -0800

    Still fixing CORS

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module Atupuerta
 
     config.middleware.insert_before 0, Rack::Cors, debug: true do
       allow do
-        origins 'http://localhost:3000'
+        origins 'http://localhost:3000, https://localhost:3000'
         resource '*', 
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head]
