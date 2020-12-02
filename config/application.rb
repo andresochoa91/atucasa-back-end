77089a1b79dbfbf66209a45fb1680dc63fbf2d36
andresochoa91 <andres_ochoa91@hotmail.com>
2020-12-02 15:57:13 -0800

    Set up

--- config/application.rb
+++ config/application.rb
@@ -28,12 +28,22 @@ module Atupuerta
 
     config.middleware.insert_before 0, Rack::Cors, debug: true do
       allow do
-        origins '*'
+        origins '
+          https://localhost:3001, 
+          http://localhost:3001,
+          localhost:3001,
+          https://atucasa.netlify.app/
+        '
         resource '*', 
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
+
+    # config.action_dispatch.default_headers = {
+    #   'Access-Control-Allow-Origin' => 'https://localhost:3001',
+    #   'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    # }
 
   end
 end
