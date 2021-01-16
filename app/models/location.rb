c44acae6040b35a39f56fa163d08250546b57587
andresochoa91 <andres_ochoa91@hotmail.com>
2021-01-16 00:15:06 -0800

    Setting up location controller with error handling Adding default images when creating users in user model

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,7 +5,7 @@ class Location < ApplicationRecord
   #   :presence => true,
   #   :on => :create
 
-  validates_length_of :country, 
+  validates_length_of :country, :state, :city,
     :minimum => 2,
     :allow_nil => true,
     :on => :update  
