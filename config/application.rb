14ad65994334c65661f0ae3e866029614f09cc74
andresochoa91 <andres_ochoa91@hotmail.com>
2021-01-27 18:41:09 -0800

    Deployin76

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module Atupuerta
     end
 
     config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Allow-Origin' => 'https://atucasa-frontend.herokuapp.com',
       'Access-Control-Request-Method' => 'GET, PATCH, PUT, POST, OPTIONS, DELETE',
       'Access-Control-Allow-Headers:' => 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     }
