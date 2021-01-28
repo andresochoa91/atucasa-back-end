3bde9bb722f1b85126cd91f9b4a7619e45a4d131
andresochoa91 <andres_ochoa91@hotmail.com>
2021-01-27 19:40:49 -0800

    Deploying9

--- config/application.rb
+++ config/application.rb
@@ -38,11 +38,12 @@ module Atupuerta
       end
     end
 
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => 'https://atucasa-frontend.herokuapp.com',
-      'Access-Control-Request-Method' => 'GET, PATCH, PUT, POST, OPTIONS, DELETE',
-      'Access-Control-Allow-Headers:' => 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    }
+    # config.action_dispatch.default_headers = {
+    #   'Access-Control-Allow-Origin' => 'https://atucasa-frontend.herokuapp.com',
+    #   'Access-Control-Allow-Origin' => 'https://atucasa.netlify.app',
+    #   'Access-Control-Request-Method' => 'GET, PATCH, PUT, POST, OPTIONS, DELETE',
+    #   'Access-Control-Allow-Headers:' => 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    # }
 
   end
 end
