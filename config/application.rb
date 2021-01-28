0b505be67daabaa48f3f936dbf4fe8b56a61adcc
andresochoa91 <andres_ochoa91@hotmail.com>
2021-01-27 18:08:19 -0800

    Deploying3

--- config/application.rb
+++ config/application.rb
@@ -39,8 +39,9 @@ module Atupuerta
     end
 
     config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => 'localhost:3001'
-      # 'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => 'GET, PATCH, PUT, POST, OPTIONS, DELETE',
+      'Access-Control-Allow-Headers:' => 'Origin, X-Requested-With, Content-Type, Accept'
     }
 
   end
